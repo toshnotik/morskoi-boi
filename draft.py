c10d63b660fc92dfe5d4b34713073849f6f89a4f
Konstantin <toshnotik666@mail.ru>
2021-12-02 12:30:55 +0300

    класс точек

--- draft.py
+++ draft.py
@@ -8,3 +8,13 @@ class Dot:
 
     def __repr__(self):
         return f'Dot({self.x}, {self.y})'
+
+
+a = Dot(1, 1)
+b = Dot(3, 2)
+c = Dot(1, 1)
+
+
+aa = [Dot(1, 1), Dot(3, 5), Dot(3, 10), Dot(1, 5)]
+
+print(a in aa)
